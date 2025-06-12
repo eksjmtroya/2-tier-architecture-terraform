cd7ac013e855fb037d0cb3a7ed9366dd72674c94
Jose Manuel Troya Valero <jmtroya@eksenergy.com>
2025-06-12 22:12:23 +0200

    vpc module

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 # store the terraform state file in s3 and lock with dynamodb
 terraform {
   backend "s3" {
-    bucket         = "bucket-2-tier" 
+    bucket         = "bucket-2-tier"
     key            = "2-tier-project/terraform.tfstate"
     region         = "eu-central-1"
     profile        = "terraform-user"
