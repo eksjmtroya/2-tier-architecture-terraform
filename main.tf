9e03e7f9561077ea9e0fdcac1cd975b2f21bec00
Jose Manuel Troya Valero <jmtroya@eksenergy.com>
2025-06-13 09:19:26 +0200

    NAT conf

--- main.tf
+++ main.tf
@@ -10,3 +10,16 @@ module "vpc" {
   pri_sub_5a_cidr = var.pri_sub_5a_cidr
   pri_sub_6b_cidr = var.pri_sub_6b_cidr
 }
+
+
+module "nat" {
+  source        = "./modules/nat"
+  pub_sub_1a_id = module.vpc.pub_sub_1a_id
+  igw_id        = module.vpc.igw_id
+  pub_sub_2b_id = module.vpc.pub_sub_2b_id
+  vpc_id        = module.vpc.vpc_id
+  pri_sub_3a_id = module.vpc.pri_sub_3a_id
+  pri_sub_4b_id = module.vpc.pri_sub_4b_id
+  pri_sub_5a_id = module.vpc.pri_sub_5a_id
+  pri_sub_6b_id = module.vpc.pri_sub_6b_id
+}
